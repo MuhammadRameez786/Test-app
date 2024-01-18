077bbc2fa5150cbfa11b5aaccf11ecec2e5413bf
MuhammadRameez786 <upwork1230@gmail.com>
2024-01-18 15:55:30 +0500

    Slider update

--- components/NFTCollection/Arts/Arts.js
+++ components/NFTCollection/Arts/Arts.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Style from "./Arts.module.css";
+import Style from "../../myCollections/MyCollections.module.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, Navigation } from 'swiper';
+import 'swiper/css'
+import 'swiper/css/pagination';
+import "swiper/css/navigation";
 import Link from "next/link";
 import { useRouter } from 'next/router';
+
+SwiperCore.use([Pagination, Navigation]);
 
 
 const Arts = () => {
@@ -25,51 +29,68 @@ const Arts = () => {
     };
   }, []);
 
-  const artsSliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    arrows: true
-  };
-
 
   return (
-    <div>
-      <Slider {...artsSliderSettings}>
-        {arts.map((collection) => (
-          <div key={collection._id} className={Style.slider_item}>
-            <Link
-              href={{
-                pathname: `/collection`,
-                query: {
-                  collectionName: collection.name,
-                  collectionImage: collection.collectionImage,
-                  collectionDescription: collection.collectionDescription,
-                },
-              }}
-            >
-              <a>
-                <img
-                  src={collection.collectionImage}
-                  alt={collection.name}
-                  className={Style.slider_image}
-                />
-                <div className={Style.slider_info}>
-                  <div className={Style.slider_name}>{collection.name}</div>
-                  <div className={Style.slider_description}>
-                    {collection.collectionDescription || 'No Discription'}
-                    <h4>Seller ID:</h4> 
-                    <p>{collection.seller.slice(0, 18)}...</p>
-                  </div>
+    <Swiper
+    slidesPerView={1}
+    spaceBetween={10}
+    pagination={{ clickable: true }}
+    breakpoints={{
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    }}
+    className={Style.slider_section}
+  >
+    {arts.map((collection) => (
+      <SwiperSlide key={collection._id}>
+        <div className={`${Style.slider_box} swiper-slide`}>
+          <Link
+            href={{
+              pathname: `/collection`,
+              query: {
+                collectionName: collection.name,
+                collectionImage: collection.collectionImage,
+                collectionDescription: collection.collectionDescription,
+              },
+            }}
+          >
+            <a>
+              <div className={Style.slider_box_img}>
+              <img 
+                src={collection.collectionImage}
+                alt={collection.name}
+                className={Style.box_main_img}
+              />
+              </div>
+              <div className={Style.slider_box_text}>
+                <div className={Style.slider_name}>{collection.name}</div>
+                <div className={Style.highest_bid}>
+                  {collection.collectionDescription || 'No Description'}
+                  <h4>Seller ID:</h4> 
+                  <p>{collection.seller.slice(0, 18)}...</p>
                 </div>
-              </a>
-            </Link>
-          </div>
-        ))}
-      </Slider>
-    </div>
-  );
+              </div>
+            </a>
+          </Link>
+        </div>
+      </SwiperSlide>
+    ))}
+    
+  </Swiper>
+);
 };
 export default Arts;
--- components/NFTCollection/Music/Music.js
+++ components/NFTCollection/Music/Music.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Style from "./Music.module.css";
+import Style from "../../myCollections/MyCollections.module.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper';
+import 'swiper/css'
+import 'swiper/css/pagination';
 import Link from "next/link";
 import { useRouter } from 'next/router';
+
+SwiperCore.use([Pagination]);
 
 
 const Music = () => {
@@ -34,10 +37,33 @@ const Music = () => {
   };
 
   return (
-    <div>
-      <Slider {...musicSliderSettings}>
-        {music.map((collection) => (
-          <div key={collection._id} className={Style.slider_item}>
+    <Swiper
+    slidesPerView={1}
+    spaceBetween={10}
+    pagination={{ clickable: true }}
+    breakpoints={{
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    }}
+    className={Style.slider_section}
+  >
+    {music.map((collection) => (
+      <SwiperSlide key={collection._id}>
+        <div className={`${Style.slider_box} swiper-slide`}>
           <Link
             href={{
               pathname: `/collection`,
@@ -49,15 +75,17 @@ const Music = () => {
             }}
           >
             <a>
-              <img
+              <div className={Style.slider_box_img}>
+              <img 
                 src={collection.collectionImage}
                 alt={collection.name}
-                className={Style.slider_image}
+                className={Style.box_main_img}
               />
-              <div className={Style.slider_info}>
+              </div>
+              <div className={Style.slider_box_text}>
                 <div className={Style.slider_name}>{collection.name}</div>
-                <div className={Style.slider_description}>
-                  {collection.collectionDescription || 'No Discription'}
+                <div className={Style.highest_bid}>
+                  {collection.collectionDescription || 'No Description'}
                   <h4>Seller ID:</h4> 
                   <p>{collection.seller.slice(0, 18)}...</p>
                 </div>
@@ -65,10 +93,10 @@ const Music = () => {
             </a>
           </Link>
         </div>
-        ))}
-      </Slider>
-    </div>
-  );
+      </SwiperSlide>
+    ))}
+  </Swiper>
+);
 };
 
 export default Music;
